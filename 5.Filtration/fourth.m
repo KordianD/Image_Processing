1f2994405e20e9c8c3d045a64c78ccb50735d07a
Kordian D <kordiandrozd@gmail.com>
2017-06-11 11:38:35 +0200

    Modified code for gradient

--- 5.Filtration/fourth.m
+++ 5.Filtration/fourth.m
@@ -87,7 +87,7 @@ subplot(1, 3, 3);
 imshow(abs(result), []);
 title('abs');
 
-figure(6;
+figure(6);
 result = uint8(conv2(double(kw), S2, 'same'));
 
 subplot(1, 3, 1)
@@ -106,7 +106,7 @@ title('abs');
 
 filter = sqrt(S1.^2 + S2.^2);
 figure(7)
-result = uint8(conv2(double(kw), fil, 'same'));
+result = uint8(conv2(double(kw), filter, 'same'));
 
 subplot(1, 3, 1);
 imshow(kw, []);
@@ -119,7 +119,7 @@ title('128')
 subplot(1, 3, 3)
 imshow(abs(result), [])
 title('abs')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 filtr = (abs(S1)+abs(S2));
 figure(8)
 result = uint8(conv2(double(kw), filtr, 'same'));
@@ -135,39 +135,39 @@ title('128')
 subplot(1, 3, 3)
 imshow(abs(result), [])
 title('abs')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 jet = imread('jet.bmp');
-% kombinowany na obrazku
 
 jet1 = conv2(double(jet), S1, 'same');
 jet2 = conv2(double(jet), S2, 'same');
 
-figure(9)
+figure(9);
 result = sqrt(jet1.^2 + jet2.^2);
 
-subplot(1, 3, 1)
-imshow(jet, [])
-title('Oryginal')
+subplot(1, 3, 1);
+imshow(jet, []);
+title('Orginal');
 
-subplot(1, 3, 2)
-imshow(result + 128, [])
-title('128')
+subplot(1, 3, 2);
+imshow(result + 128, []);
+title('128');
 
-subplot(1, 3, 3)
-imshow(abs(result), [])
-title('abs')
+subplot(1, 3, 3);
+imshow(abs(result), []);
+title('abs');
 
-figure(10)
+figure(10);
 result = abs(jet1) + abs(jet2);
 
-subplot(1, 3, 1)
-imshow(jet, [])
-title('Oryginal')
+subplot(1, 3, 1);
+imshow(jet, []);
+title('Orginal');
 
-subplot(1, 3, 2)
-imshow(result + 128, [])
-title('128')
+subplot(1, 3, 2);
+imshow(result + 128, []);
+title('128');
 
-subplot(1, 3, 3)
-imshow(abs(result), [])
-title('abs')
+subplot(1, 3, 3);
+imshow(abs(result), []);
+title('abs');
